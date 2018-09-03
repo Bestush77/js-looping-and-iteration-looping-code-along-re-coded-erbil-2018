570066c4cae152dceb6c197b672cc64e2f1fd0d6
Bestun Gardi <bestun95@yahoo.com>
2018-09-03 08:53:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+function printBadges (names) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`);
+  }
+
+  return names;
+}
+
 function tailsNeverFails () {
   let tails = 0;
 
